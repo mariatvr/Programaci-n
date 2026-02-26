b13d999a47e96d6f5689f5ba857c0f3681b6e41d
mtervr <mterv12@hotmail.com>
2026-02-26 08:23:13 +0100

    Termino la clase planeta

--- Ejercicio1Tarea4/Planeta.java
+++ Ejercicio1Tarea4/Planeta.java
@@ -4,4 +4,14 @@ public class Planeta extends CuerpoCeleste{
     public Planeta(String nombre, double periodoOrbital) {
         super(nombre, periodoOrbital, TipoCuerpoCeleste.PLANETA);
     }
+
+    @Override
+    public boolean addSatelite(CuerpoCeleste satelite){
+        if (satelite.getTipoCuerpo()==TipoCuerpoCeleste.LUNA){
+            return super.addSatelite(satelite);
+        } else {
+            System.out.println("Error: A un planeta solo se le pueden añadir lunas.");
+            return false;
+        }
+    }
 }
